5a7922e482d8ac73f49aaa16ebf7a223091cba6a
Anton Chepelev <chepelash@gmail.com>
2022-06-14 13:41:07 +0300

    Completed task

--- program/Program.cs
+++ program/Program.cs
@@ -1,1 +1,52 @@
-﻿
+﻿using System;
+
+const int LEN_LIMIT = 3;
+
+/// <summary>
+/// reads a string from terminal and returns false on error and string array
+/// </summary>
+bool grabStrings(ref string[] stringArray){    
+    Console.Write("Enter words separated by a space > ");
+    string? someString = Console.ReadLine();
+    if(null == someString)
+        return false;
+    stringArray = someString.Split(" ");
+    return true;
+}
+
+
+/// <summary>
+/// main task
+/// </summary>
+void homework(){
+    string[] stringArray = {};
+    string[] resultArray = {};    
+    int lenLimit = LEN_LIMIT;
+
+    if(!grabStrings(ref stringArray)){
+        Console.WriteLine("Error in reading");
+        return;
+    }
+
+    int cntr = 0;
+    for(int i = 0; i < stringArray.Length; i++){
+        if(null == stringArray[i])
+            continue;
+        if(stringArray[i].Length <= lenLimit){
+            Array.Resize(ref resultArray, resultArray.Length + 1);
+            resultArray[cntr] = stringArray[i];
+            cntr++;
+        }
+    }
+
+    for(int i = 0; i < resultArray.Length; i++){
+        Console.Write($"{resultArray[i]} ");
+    }
+    Console.WriteLine("");
+}
+
+void Main(){
+    homework();
+}
+
+Main();
